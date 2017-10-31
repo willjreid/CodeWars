120eb307de039956ea25ced0d05fc0b46b839a41
William J Reid <willjreid@gmail.com>
2017-10-31 13:40:23 -0700

    complex solution

---
+++ NiceArray/readme.md
@@ -0,0 +1,5 @@
+This Kata deals with the ability to evaluate an array as been "nice" or not ("naughty?") based on whether each numerical value in the array has another value in the array that is 1 higher or 1 lower.
+
+Dre and I whiteboarded a solution involving looping through the array.
+
+The problem can be found at https://www.codewars.com/kata/nice-array/train/javascript
---
+++ NiceArray/solution.js
@@ -0,0 +1,22 @@
+function isNice(arr){
+  arr.sort(function(a,b) {return b - a});
+  let hi = [];
+  let lo = [];
+  for (i = 0; i < arr.length - 1; i++) {
+    hi.push(arr[i + 1] - arr[i]);
+    lo.push(arr[i] - arr[i + 1]);
+  };
+  if (hi.contains = function(hi) {
+    for (i = 0; i < hi.length; i++);
+    i = 1;
+  }) return true ;
+  if (lo.contains = function(lo) {
+    for (i = 0; i < lo.length; i++);
+    i = 1;
+  }) return true;
+}
+
+//solution 2
+function isNice(arr) {
+  arr.every = function(each, )
+}
